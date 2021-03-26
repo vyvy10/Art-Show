fe0bc4bfd70220ca1f364ac5a28ff46479cf8ac4
vyvy10 <79157046+vyvy10@users.noreply.github.com>
2021-03-26 19:06:28 -0400

    Update coin.js

--- coin.js
+++ coin.js
@@ -1,13 +1,17 @@
-class Coin{
-  constructor(){
+class Coin {
+  constructor() {
+    //The size
     this.r = 100;
+    //X starts of the edge
     this.x = width;
+    //Y starts at the bottom
     this.y = height - this.r;
   }
-  move(){
+  move() {
+    //The whole screen is scrolling by 5 pixels
     this.x -= 5;
   }
-  show(){
+  show() {
     image(cImg, this.x, this.y - 90, this.r - 50, this.r - 50);
   }
 }
