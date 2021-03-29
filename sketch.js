8c1feecaa8ba31a3744be3a105b0d629b1b30588
vyvy10 <79157046+vyvy10@users.noreply.github.com>
2021-03-29 11:26:39 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ function preload() {
   wImg = loadImage("whale.png");
   pImg = loadImage("plasticbag.png");
   cImg = loadImage("coin.jpeg");
-  //mySound = loadSound("_natureocean.mp3")
+  
   
 }
 
@@ -21,8 +21,7 @@ function setup() {
   bckgrnd = loadImage("ocean(1).jpg");
   bckgrnd2 = loadImage("ocean2.jpeg");
   scene = 1;
- //mySound.play();
-//mySound.setVolume(0.6);
+ 
 
 
 
@@ -80,8 +79,7 @@ function draw() {
     textSize(18);
     text("WELCOME TO THE MARINE MISSION!", 10, 60);
     text("Let's try to protect our whale!", 70, 100);
-   // mySound.play();
-// mySound.setVolume(0.02);
+  
   }
   
   theButton();
